68288affae03b4e549535b9237a2890214bad627
Albrey Brown <albrey.brown@gmail.com>
2014-10-20 22:06:45 -0700

    set up dependencies and gulpfile

--- gulpFile.js
+++ gulpFile.js
@@ -1,29 +1,36 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')({lazy: false}) // grabs all node modules prepended
 // with 'gulp' and adds them to the $ object.
-var client = {
-	root: './client',
-	index: './client/index.html',
-	scripts: ['./client/app/**/*.js', '!./client/app/modules/**/*.js'] // '**' is any directory, '*' any file
+
+
+var client = { // all of the roots for our directories relative to our gulp file
+	root: './client', // this makes our code cleaner/easier to write
+	index: './client/index.html', // grouping files by folder
+	scripts: ['./client/app/**/*.js', '!./client/app/modules/**/*.js'], // '**' is any directory, '*' any file
+	css: './client/app/styles/*.css',
 }
 
 var options = {
 	inject: {
 		app: {
-			relative: true,
-			name: 'app',
+			relative: true, // looking up what $sequence does
+			name: 'app', // name of directory
 		},
 		modules: {
 			relative: true,
 			name: 'modules',
-		}
+		},
+		css: {
+			relative: true,
+			name: 'styles',
+		},
 	},
 }
 
-gulp.task('default', ['dev']);
-gulp.task('dev', $.sequence('inject', 'server'));
-gulp.task('server', server);
-gulp.task('inject', inject);
+gulp.task('default', ['dev']); // defaults so you  only have to type 'gulp' in to the terminal to run whatevers in the array
+gulp.task('dev', $.sequence('inject', 'server')); // uses sequence to run tasks in order
+gulp.task('server', server); // runs the server in a separate functions
+gulp.task('inject', inject); // runs an inject functions
 
 
 
@@ -36,10 +43,14 @@ function server(){
 function inject(){
 	var scripts = gulp.src(client.scripts, {read:false});
 	var modules = gulp.src('./client/app/modules/**/*.js', {read:false});
-	// read: false keeps stream
+	var css = gulp.src(client.css, {read:false});
+	// var bootstrap = gulp.src('./client/app/www/lib/bootstrap/dist/bootstrap-theme.min.css', {read:false});
+	// 'read: false' keeps stream
 	// from reading entire file. All we want is path.
 	return gulp.src(client.index)
-		.pipe($.inject(scripts, options.inject.app))
+		.pipe($.inject(scripts, options.inject.app)) // pipe allows you to add multiple processes to your gulp stream
 		.pipe($.inject(modules, options.inject.modules)) // with inject, you include the stream to the files to be injected
+		.pipe($.inject(css, options.inject.css))
+		// .pipe($.inject(bootstrap, options.inject.css))
 		.pipe(gulp.dest(client.root)); // dest is the destination of the stream - should be last
 }
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var app = express();
+var app = express(); // invokes the express applications
 
 // express.static serves up the index.html file in client.
-app.use(express.static('./client'));
+app.use(express.static('./client')); // look up app.use
 
 app.listen(9000, function(){ // creates the server
 	console.log('serve listening on 9000;');
